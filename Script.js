42a21dbfc1d2f486dadf993ec2947ad42cc12803
mikeaf353 <157494750+mikeaf353@users.noreply.github.com>
2025-09-29 13:29:35 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('hello-btn').addEventListener('click', () => {
+  alert('You clicked the button! 🎉');
+});
